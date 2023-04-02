291be2c30c383b516cf4f617fae3560e5232945b
mak-1997 <107460834+mak-1997@users.noreply.github.com>
2023-04-02 17:32:28 +0530

    added admin login and signup routes

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,19 +1,65 @@
 const { UserModel } = require("../model/user.model");
 const { ProductModel } = require("../model/product.model");
+const { AdminModel } = require("../model/admin.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // Admin Signup
-const adminSignup = async(req,res) => {
-    
+const adminSignup = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        const admin = await AdminModel.findOne({ email });
+        if (!admin) {
+            bcrypt.hash(password, 3, async (err, hash) => {
+                if (err) {
+                    res.status(400).send({ "msg": err.message });
+                }
+                else {
+                    const newAdmin = new AdminModel({ name, email, password: hash });
+                    await newAdmin.save();
+
+                    res.status(200).send({ "msg": "Registration Successfull !!" });
+                }
+            });
+        }
+        else {
+            res.status(400).send({ "msg": "Admin already exists. Please login." });
+        }
+    } catch (error) {
+        res.status(400).send({ "msg": error.message });
+    }
+}
+
+// Admin Login 
+const adminLogin = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const admin = await AdminModel.findOne({ email });
+        if (admin) {
+            bcrypt.compare(password, admin.password, (err, result) => {
+                if (result) {
+                    res.status(200).send({ "msg": "Login Successful !!", "name": admin.name, "token": jwt.sign({ "adminID": admin._id }, "admin_signature") });
+                }
+                else {
+                    res.status(400).send({ "msg": "Wrong password !!" });
+                }
+            })
+        } else {
+            res.status(400).send({ "msg": "Admin not found, Please Check your email !!" });
+        }
+    } catch (error) {
+        res.status(400).send({ "msg": error.message });
+    }
+
 }
 
 //  Add new product
-const addProduct = async(req,res)=> {
+const addProduct = async (req, res) => {
     // Admin token check
     const payload = req.body;
     try {
-        const newProd = new ProductModel({...payload});
+        const newProd = new ProductModel({ ...payload });
         await newProd.save();
         res.status(200).send(newProd);
     } catch (error) {
@@ -21,18 +67,13 @@ const addProduct = async(req,res)=> {
     }
 }
 
-// Admin Login 
-const adminLogin = async(req,res) => {
-
-}
-
 // Edit product details
 const editProduct = async (req, res) => {
     // Admin token check
-    const  prodID  = req.params.prodID;
+    const prodID = req.params.prodID;
     const payload = req.body;
     try {
-        const product = await ProductModel.findByIdAndUpdate({ _id: prodID }, payload,{new: true});
+        const product = await ProductModel.findByIdAndUpdate({ _id: prodID }, payload, { new: true });
         if (!product) {
             return res.status(404).send({ msg: "Product not found" });
         }
@@ -45,7 +86,7 @@ const editProduct = async (req, res) => {
 // Delete a product
 const deleteProduct = async (req, res) => {
     // Admin token check
-    const  prodID  = req.params.prodID;
+    const prodID = req.params.prodID;
     try {
         const deletedProduct = await ProductModel.findByIdAndDelete({ _id: prodID });
         if (!deletedProduct) {
@@ -75,4 +116,4 @@ const deleteUser = async (req, res) => {
 }
 
 
-module.exports = { editProduct, deleteProduct, deleteUser, addProduct }
+module.exports = { editProduct, deleteProduct, deleteUser, addProduct, adminSignup, adminLogin }
